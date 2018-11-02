3a1c2f38bb8e35225fd70715a82dce04d9b300bc
bde <bde@lesfurets.com>
2018-11-02 10:30:25 +0100

    Move vehicles filtering to container

--- src/Utils.js
+++ src/Utils.js
@@ -1,1 +1,9 @@
 export const uniqueSortedItems = (items) => [...new Set(items)].sort();
+
+export const filterVehicules = (vehicules, marque, modele, boite, carburant, carrosserie, puissance) => vehicules
+    .filter(vehicule => marque === "all" || vehicule.marqueLibelle === marque)
+    .filter(vehicule => modele === "all" || vehicule.modeleLibelle === modele)
+    .filter(vehicule => boite === "all" || vehicule.boiteVitesses === boite)
+    .filter(vehicule => carburant === "all" || vehicule.energieLibelle === carburant)
+    .filter(vehicule => carrosserie === "all" || vehicule.carrosserieLibelle === carrosserie)
+    .filter(vehicule => puissance === "all" || vehicule.puissance === puissance);
--- src/containers/VehiculesContainer.js
+++ src/containers/VehiculesContainer.js
@@ -1,10 +1,19 @@
 import {connect} from "react-redux";
+import {filterVehicules} from "../Utils";
 import VehiculesTable from "../components/VehiculesTable";
 
 
 const mapStateToProps = state => {
     return {
-        vehicules: state.vehiculier.vehicules
+        vehicules: filterVehicules(
+            state.vehiculier.vehicules,
+            state.vehiculier.selectedMarque,
+            state.vehiculier.selectedModele,
+            state.vehiculier.selectedBoite,
+            state.vehiculier.selectedCarburant,
+            state.vehiculier.selectedCarrosserie,
+            state.vehiculier.selectedPuissance,
+        )
     };
 };
 
